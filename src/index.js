d1f18348efffc0b4f2dc06b72835c1569d9e9051
Pankaj Makwana <imail2pankaj@gmail.com>
2021-05-25 16:39:29 +0530

    Updated code with bug fixes

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+// const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
@@ -19,9 +19,9 @@ const rootReducer = combineReducers({
     auth: authReducer
 });
 
-const store = createStore(rootReducer, composeEnhancers(
+const store = createStore(rootReducer, 
     applyMiddleware(thunk)
-));
+);
 
 const app = (
     <Provider store={store}>
